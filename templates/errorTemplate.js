2eb2e4ec7aee6f8159d9e297226ad399891a5bcd
Felice Forgione <forgione67@gmail.com>
2023-04-21 23:44:25 -0400

    Added jsdoc comments and created documentation html

--- templates/errorTemplate.js
+++ templates/errorTemplate.js
@@ -1,3 +1,17 @@
+/**
+ * Provides a function to create error responses
+ * @module Error Template
+ */
+
+/**
+ * Template to provide error responses
+ *
+ * @param {Response} res Express Response Object
+ * @param {Error} err Error Object
+ * @param {string} message Message about error
+ * @param {number} status Response status code
+ * @returns {Object}
+ */
 const errorTemplate = (res, err, message = "none", status = 501) => {
   return res.status(status).json({
     error: {
--- templates/successTemplate.js
+++ templates/successTemplate.js
@@ -1,3 +1,19 @@
+/**
+ * Provides a function to create success responses
+ * @module Success Template
+ */
+
+/**
+ * Templete to provide success responses
+ *
+ * @param {Resonse} res Express Response object
+ * @param {Object} result Result from API
+ * @param {string} message Message about success response
+ * @param {number} status Status code
+ * @param {boolean} loggedIn User is logged in
+ * @param {string} token JWT token
+ * @returns {Object}
+ */
 const successTemplate = (
   res,
   result,
